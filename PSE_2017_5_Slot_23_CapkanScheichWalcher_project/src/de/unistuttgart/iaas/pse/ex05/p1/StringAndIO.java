325346cb622945367e9725e0182581f077fb79ff
Florian <33514818+Acaledus@users.noreply.github.com>
2017-12-07 15:23:00 +0100

    Aufgabe 1 fast gelöst.
    Die HashMap funktioniert noch nicht ganz.

--- PSE_2017_5_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex05/p1/StringAndIO.java
+++ PSE_2017_5_Slot_23_CapkanScheichWalcher_project/src/de/unistuttgart/iaas/pse/ex05/p1/StringAndIO.java
@@ -1,8 +1,6 @@
 package de.unistuttgart.iaas.pse.ex05.p1;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -10,55 +8,90 @@ import java.util.Scanner;
 
 /**
  * Offers various text processing methods.
- * 
- * @author Sandro Speth
- * @version 1.0
+ *
+ * @author Florian Walcher
  */
 public class StringAndIO {
 
 	/**
 	 * Reads text from a file and counts the occurrence of each word. The result
 	 * (i.e. which words occur in the file and how often) is print to the console.
-	 * 
+	 *
 	 * @param fileName
 	 *            The name of the file to read from (may include a relative or
 	 *            absolute path)
 	 */
 	public static void readTextAndCount(String fileName) {
-		// TODO
+		Map<String, Number> woerter = new HashMap<String, Number>();
+		Scanner sc = new Scanner(fileName);
+		while(sc.hasNext()) {
+			if(woerter.containsKey(sc.next())) {
+				woerter.put(sc.next(), woerter.get(sc.next()).intValue() + 1);
+			} else {
+				woerter.put(sc.next(), 0);
+			}
+		}
+		sc.close();
+		System.out.println(woerter.toString());
 	}
 
 	/**
 	 * Checks if a character is a vowel.
-	 * 
+	 *
 	 * @param c
 	 *            The character to check
 	 * @return True if the character is a vowel
 	 */
 	public static boolean isVowel(char c) {
-		// TODO
-		return false;
+		switch(c) {
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+			case 'ä':
+			case 'ö':
+			case 'ü':
+			case 'A':
+			case 'E':
+			case 'I':
+			case 'O':
+			case 'U':
+			case 'Ö':
+			case 'Ü':
+			case 'Ä':
+				return true;
+			default: return false;
+		}
 	}
 
 	/**
 	 * Translates a string into Pig Latin by adding "pi" after each vowel.
-	 * 
+	 *
 	 * @param text
 	 *            Text to translate
 	 * @return returns Text translated into Pig-Latin
 	 */
 	public static String toPigLatin(String text) {
-		// TODO
-		return null;
+		StringBuilder pig = new StringBuilder(text);
+		for (int i = text.length() - 1; i >= 0; i--) {
+			if(isVowel(text.charAt(i))) {
+				if(i == text.length() - 1 || (isVowel(text.charAt(i+1)) == false)) {
+					pig.insert(i + 1, "pi");
+				}
+			}
+		}
+		return pig.toString();
 	}
 
 	/**
 	 * only there for testing purpose
 	 */
 	public static void main(String[] args) {
+		System.out.println(toPigLatin("aeiou"));
 		System.out.println(toPigLatin("Meine Mutter isst gerne Fisch."));
 		System.out.println("----------------------------------------");
 		readTextAndCount("beispieltext.txt");
 	}
-	
+
 }
